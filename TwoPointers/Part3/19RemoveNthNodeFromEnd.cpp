41bf954e66186a49da72795bca31d8c207dfaab0
Raghav Chhabra <rchhabra_be23@thapar.edu>
2026-03-31 15:11:57 +0530

    Update 19RemoveNthNodeFromEnd.cpp

--- TwoPointers/Part3/19RemoveNthNodeFromEnd.cpp
+++ TwoPointers/Part3/19RemoveNthNodeFromEnd.cpp
@@ -1,3 +1,6 @@
+
+// M1 -> By finding the length of the LinkedList which is good method but the follow up question is asked that u are not allowed to find length
+
 class Solution {
 public:
     int sizecount(ListNode* head){
@@ -19,6 +22,28 @@ public:
             temp = temp->next;
         }
         temp->next = temp->next->next;
+        return head;
+    }
+};
+
+// Without finding the length of the Linked List :-
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* ptr = head , *temp = head;                
+
+        for(int i=0 ; i<n ; i++) ptr = ptr->next;           // moving the ptr to n times
+    
+        if(!ptr) return head->next;                        // if the ptr becomes NULL which means the start node we have to delete
+
+        while(ptr->next){                        // move ptr and temp each step by step (1 step at a time)
+            ptr = ptr->next;
+            temp = temp->next;    
+        }
+
+        temp->next = temp->next->next ;             // temp->next node will be the node which we have to delete 
+
         return head;
     }
 };
