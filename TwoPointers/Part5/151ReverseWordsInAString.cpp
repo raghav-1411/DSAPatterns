e4582030695b791dd57e3579a44e90e07cfd72ed
Prachi <pprachi_be23@thapar.edu>
2026-04-02 18:35:47 +0530

    Update 151ReverseWordsInAString.cpp

--- TwoPointers/Part5/151ReverseWordsInAString.cpp
+++ TwoPointers/Part5/151ReverseWordsInAString.cpp
@@ -1,1 +1,31 @@
+// Approach 1 -> 
 
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> v;
+        string t = "";
+        for(int i = 0 ; i<s.size() ; i++){
+            if(s[i]!=' '){
+                t+=s[i];
+            }
+            else{
+                v.push_back(t);
+                t = "";
+            }
+        }
+        v.push_back(t);
+        vector<string> ans;
+        for(int i=0 ; i<v.size() ; i++){
+            if(v[i]!="") ans.push_back(v[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        s = "";
+        for(int i=0 ; i<ans.size() ; i++){
+            s+=ans[i];
+            s+=' ';
+        }
+        s.pop_back();
+        return s;
+    }
+};
