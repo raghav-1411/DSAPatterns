59948f8640b2fee424e6e43f08ad3a9478df7440
Raghav Chhabra <rchhabra_be23@thapar.edu>
2026-04-02 22:39:12 +0530

    Update 151ReverseWordsInAString.cpp

--- TwoPointers/Part5/151ReverseWordsInAString.cpp
+++ TwoPointers/Part5/151ReverseWordsInAString.cpp
@@ -27,3 +27,76 @@ public:
 
     }
 };
+
+// Approach 2 ->
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        int l = 0 , r = s.size()-1;
+
+        while(s[l]==32) l++;
+        while(s[r]==32) r--;
+
+        for(int i=l ; i<=r ; i++){
+            if(s[i]!=32) ans += s[i];
+            else{
+                if(ans[ans.size()-1]!=32) ans += s[i];
+            }
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        int i = 0 , j = 0;
+
+        while(j<ans.size()){
+            if(ans[j]==32){
+                reverse(ans.begin()+i,ans.begin()+j);
+                j++;
+                i = j ;
+            }
+            else j++;
+        }
+        reverse(ans.begin()+i,ans.end());
+        return ans;
+
+    }
+};
+
+// Best Approach -> In place sorting :-
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+
+        reverse(s.begin(), s.end());
+
+        int i = 0, j = 0;
+
+        while (j < n) {
+            while (j < n && s[j] == ' ') j++;
+
+            if (j >= n) break;
+
+            if (i > 0){                // because we want one space after each word we are doing it before because afterwards we will do reverse
+                s[i] = ' ';
+                i++;
+            }
+
+            int start = i;
+            while (j < n && s[j] != ' ') {
+                s[i] = s[j];
+                i++;
+                j++;
+            }
+
+            reverse(s.begin() + start, s.begin() + i);
+        }
+        
+        s.resize(i);        // limit the string to size i!! 
+
+        return s;
+    }
+};
