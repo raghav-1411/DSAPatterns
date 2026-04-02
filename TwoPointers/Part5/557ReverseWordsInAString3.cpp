90970fd7f893144af25b39a06fcae368bf0f42c5
Raghav Chhabra <rchhabra_be23@thapar.edu>
2026-04-02 22:49:17 +0530

    Create 557ReverseWordsInAString3.cpp

---
+++ TwoPointers/Part5/557ReverseWordsInAString3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int i = 0 , j = 0 ; 
+
+        while(j<s.size()){
+            if(s[j]==32){
+                reverse(s.begin()+i,s.begin()+j);
+                j++;
+                i = j ;
+            }
+            else j++;
+        }
+        reverse(s.begin()+i,s.begin()+j);
+        return s;
+    }
+};
