2e88f4d801f90486b572f9a1105632315561b0ec
Raghav Chhabra <rchhabra_be23@thapar.edu>
2026-04-08 23:00:43 +0530

    Create 1838FrequencyOfTheMostFrequentElement.cpp

---
+++ SlidingWindow/Part2/1838FrequencyOfTheMostFrequentElement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        long long sum = 0 ;
+        int maxLength = 0 ; 
+        int i = 0 , j = 0 ;
+
+        while(j<nums.size()){
+            sum += nums[j];
+
+            long long total = 1LL * (j-i+1) * nums[j];
+
+            if(total-sum>k){
+                sum -= nums[i];
+                i++;
+            }
+            maxLength = max(maxLength,j-i+1);
+            j++;
+
+        }
+
+        return maxLength;
+    }
+};
