4d94a938e4103db23a1a885f6f32e82d5453b1c4
Raghav Chhabra <rchhabra_be23@thapar.edu>
2026-04-14 12:55:05 +0530

    Update 424LongestRepeatingCharacterReplacement.cpp

--- SlidingWindow/Part2/424LongestRepeatingCharacterReplacement.cpp
+++ SlidingWindow/Part2/424LongestRepeatingCharacterReplacement.cpp
@@ -53,3 +53,33 @@ public:
         return ans;
     }
 };
+
+// Goat Approach :-
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int i = 0, j = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        while (j < s.size()) {
+            
+            freq[s[j] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[j] - 'A']);
+
+            while ((j - i + 1) - maxFreq > k) {
+                freq[s[i] - 'A']--;
+                i++;
+            }
+
+            ans = max(ans, j - i + 1);
+
+            j++;
+        }
+
+        return ans;
+    }
+};
